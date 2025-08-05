e26e9998e54a43d068fdc122467a39ab3645f08f
Tien Nguyen <tiencp1234hd@gmail.com>
2025-08-05 10:51:23 +0700

    Update Swiper

--- src/app/components/banner/banner.tsx
+++ src/app/components/banner/banner.tsx
@@ -1,20 +1,52 @@
+'use client'
 import Image from 'next/image';
 import './banner.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
 export default function Banner() {
+    const banners = [
+  {
+    id: 1,
+    image: '/Promo1.webp'
+  },
+  {
+    id: 2,
+    image: '/Promo2.jpg'
+  },
+  {
+    id: 3,
+    image: '/Promo3.jpg'
+  },
+];
     return (
-        <div className="banner">
-            <div className="content">
-                <h1>Taste the delicous</h1>
-                <p>Taste the best organic fruits harvested with the best possible quality, obtaining unique and delicious flavors.</p>
-                <button>Xem chi tiet</button>
-            </div>
-            <Image
-                src="/Promo.png"
-                alt="Banner Image"
-                width={800}
-                height={600}
-                className="banner-image"
-            />
-        </div>
+        <div className='slideShow'>
+            <Swiper
+            modules={[Autoplay, Pagination]}
+            spaceBetween={0}
+            slidesPerView={1}
+            loop={true}
+            autoplay={{
+                delay: 4000,
+                disableOnInteraction: false,
+            }}
+            pagination={{ clickable: true }}
+            className="banner-swiper"
+        >
+        {banners.map((item) => (
+            <SwiperSlide className='image-wrapper' key={item.id}>
+                    <Image
+                    src={item.image}
+                    alt={"banner_img"}
+                    width={1600}
+                    height={100}
+                    className="banner-image"
+                    style={{ objectFit: 'cover' }}
+                    />
+            </SwiperSlide>
+            ))}
+            </Swiper>
+        </div> 
     )
 }
