80c1402e7e50ccd892e6ad4814936be9f305234d
NGUYEN MINH TIEN <144932728+TienNguyenTLU@users.noreply.github.com>
2025-08-12 15:43:27 +0700

    refactor: update food list and card for store

--- src/app/components/foodlist/foodcard.tsx
+++ src/app/components/foodlist/foodcard.tsx
@@ -1,13 +1,28 @@
+import Image from 'next/image'
 import './foodcard.css'
-export default function FoodCard({food}) {
+
+interface Food {
+    _id: number
+    name: string
+    price: number
+    image: string
+}
+
+export default function FoodCard({ food }: { food: Food }) {
     return (
         <div className="food-card">
-            <img src={food.image} alt={food.name} className="food-image" />
+            <Image
+                src={food.image}
+                alt={food.name}
+                width={210}
+                height={160}
+                className="food-image"
+            />
             <div className="food-details">
                 <h2 className="food-name">{food.name}</h2>
-                <h3 className="food-price">${food.price}</h3>
+                <h3 className="food-price">${food.price.toFixed(2)}</h3>
             </div>
             <button className="add-to-cart">Buy</button>
         </div>
-    );
+    )
 }
--- src/app/components/foodlist/foodlist.tsx
+++ src/app/components/foodlist/foodlist.tsx
@@ -1,27 +1,21 @@
 'use client'
 import FoodCard from "./foodcard"
-import './foodlist.css';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import { Autoplay, Navigation } from "swiper/modules";
-export default function FoodList()
-{
-    const mockFoods = [
-  { _id: 1, name: "Nâu đá", price: 1.00, image: "./1.webp" },
-  { _id: 2, name: "Đen đá", price: 1.00, image: "./2.webp" },
-  { _id: 3, name: "Bạc xỉu", price: 2.00, image: "./3.webp" },
-  { _id: 4, name: "Caramel", price: 2.5, image: "./4.webp" },
-  { _id: 5, name: "Americano", price: 3.0, image: "./5.webp" },
-];
+import './foodlist.css'
+
+export default function FoodList() {
+    const foods = [
+        { _id: 1, name: "Nâu đá", price: 1.0, image: "/1.webp" },
+        { _id: 2, name: "Đen đá", price: 1.0, image: "/2.webp" },
+        { _id: 3, name: "Bạc xỉu", price: 2.0, image: "/3.webp" },
+        { _id: 4, name: "Caramel", price: 2.5, image: "/4.webp" },
+        { _id: 5, name: "Americano", price: 3.0, image: "/5.webp" },
+    ]
+
     return (
         <div className="foodlist">
-             <ul>
-                {mockFoods.map((item) => 
-                (
-                    <FoodCard key={item._id} food={item}/>
-                ))}
-             </ul>
+            {foods.map((item) => (
+                <FoodCard key={item._id} food={item} />
+            ))}
         </div>
     )
 }
